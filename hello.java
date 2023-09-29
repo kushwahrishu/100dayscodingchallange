0f4a21467e6a24b862babffcc5fa0840f9458543
kushwahrishu <145997382+kushwahrishu@users.noreply.github.com>
2023-09-30 00:08:52 +0530

    Update day1.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello java i will be consistent for next 100 days!");
     }
 }
